0bfef9717440cc0d35054b33b6a656409b9a04e6
Hung Tang <hungtang@hotmail.com>
2021-04-29 10:08:44 -0700

    completed login page entries and validated that the requests were sent successfully

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,3 +1,27 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        // check the response status
+        if (response.ok) {
+            console.log('success');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
 async function signupFormHandler(event) {
     event.preventDefault();
 
@@ -25,4 +49,5 @@ async function signupFormHandler(event) {
 }
 
 
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
